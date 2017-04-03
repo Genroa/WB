c42ee1e1eecec63e0a9a754206c85a85f312ee79
Genroa <genroa@hotmail.fr>
2017-04-04 01:13:48 +0200

    premières classes

---
+++ src/main/Bot.java
@@ -0,0 +1,9 @@
+package main;
+
+/**
+ * Cette classe représente le bot en lui-même. Les opérations de haut niveau
+ * (charger un comportement par exemple) sont écrites ici.
+ */
+public class Bot {
+
+}
